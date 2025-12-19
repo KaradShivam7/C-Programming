8916c197efa65590515cebf82f8137d135e4b246
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-19 21:52:06 +0530

    positive-negative number

---
+++ conditional_statement/positive_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+    if(n > 0)
+        printf("Positive");
+    else if(n < 0)
+        printf("Negative");
+    else
+        printf("Zero");
+    return 0;
+}
