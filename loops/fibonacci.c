575eee8c589f71eb5404f8b7b197b0e7fa681c1c
KaradShivam7 <shivamkarad20@gmail.com>
2025-12-20 22:51:03 +0530

    fibonacci

---
+++ loops/fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10, t1 = 0, t2 = 1, next;
+    printf("%d %d ", t1, t2);
+    for(int i = 3; i <= n; i++) {
+        next = t1 + t2;
+        printf("%d ", next);
+        t1 = t2;
+        t2 = next;
+    }
+    return 0;
+}
